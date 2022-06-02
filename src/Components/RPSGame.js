98996dae22fd57ad45699f921eb7b02258767b9c
SiegeTank-90 <ccline140@yahoo.com>
2022-06-02 17:42:52 -0500

    Updating Files To Work On At Home. Current Problem react_devtools_backend.js:4026 Warning: Cannot update a component (`App`) while rendering a different component (`PlayAgain`). To locate the bad setState() call inside `PlayAgain`, follow the stack trace as described in https://reactjs.org/link/setstate-in-render     at PlayAgain (http://localhost:3000/static/js/bundle.js:547:13)     at div     at Game (http://localhost:3000/static/js/bundle.js:665:68)     at div     at App (http://localhost:3000/static/js/bundle.js:427:84) & Repeating Random Number

--- src/Components/RPSGame.js
+++ src/Components/RPSGame.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import PlayAgain from "./PlayAgain.js";
 import Symbol from "./Symbol.js";
 import SelBG from "../images/bg-triangle.svg";
 
-function Game() {
+function Game(prop) {
   const GameObjects = {
     Rock: {
       id: 1,
@@ -20,22 +21,114 @@ function Game() {
       img: "./images/icon-scissors.svg"
     }
   };
+  var [ID, setID]  = useState(Math.floor(Math.random() * 3) + 1);
+  var Computer = ComputerPick();
 
-  function Selected(props) {
-    console.log(props);
+  function ComputerPick() {
+    if (1 === ID) {
+      return GameObjects.Rock;
+    } else if (2 === ID) {
+      return GameObjects.Paper;
+    } else if (3 === ID) {
+      return GameObjects.Sissors;
+    }
   }
 
-  return (
-    <>
-      
-      <div className="FlexContainer">
+  function CalculateWinner(player, computer) {
+    if (player === 1 && computer === 3) {
+      return "You Win";
+      // Rock
+    } else if (player === 2 && computer === 1) {
+      return "You Win";
+      //Paper
+    } else if (player === 3 && computer === 2) {
+      return "You Win";
+      //Siscores
+    } else if (player === computer) {
+      return "Draw";
+    } else {
+      return "You Lose";
+    }
+  }
+
+  function Selected(props) {
+    setGameState(
+      <div className="MatchContainer">
+        <Symbol choice={props} Selected={Selected} />
+        <div className="EmptySymbol"></div>
+      </div>
+    );
+
+    setTimeout(() => {
+      if (CalculateWinner(props.id, ID) === "You Win") {
+        setGameState(
+          <div className="MatchContainer">
+            <Symbol choice={props} Selected={""} />
+            <PlayAgain
+              Title="You Win"
+              Reset={Reset}
+              Score={prop.Score}
+              setScore={prop.setScore}
+            />
+            <Symbol choice={Computer} Selected={""} />
+          </div>
+        );
+      } else if (CalculateWinner(props.id, ID) === "You Lose") {
+        setGameState(
+          <div className="MatchContainer">
+            <Symbol choice={props} Selected={""} />
+            <PlayAgain
+              Title="You Lose"
+              Reset={Reset}
+              Score={prop.Score}
+              setScore={prop.setScore}
+            />
+            <Symbol choice={Computer} Selected={""} />
+          </div>
+        );
+      } else if (CalculateWinner(props.id, ID) === "Draw") {
+        setGameState(
+          <div className="MatchContainer">
+            <Symbol choice={props} Selected={""} />
+            <PlayAgain
+              Title="Draw"
+              Reset={Reset}
+              Score={prop.Score}
+              setScore={prop.setScore}
+            />
+            <Symbol choice={Computer} Selected={""} />
+          </div>
+        );
+      }
+    }, 1500);
+  }
+
+  const defaultState = (
+    <div className="GridContainer">
+      <div className="Grid-4">
         <img className="SelectBackground" src={SelBG} />
+      </div>
+      <div className="Grid-1">
         <Symbol choice={GameObjects.Rock} Selected={Selected} />
+      </div>
+      <div className="Grid-2">
         <Symbol choice={GameObjects.Paper} Selected={Selected} />
+      </div>
+
+      <div className="Grid-3">
         <Symbol choice={GameObjects.Sissors} Selected={Selected} />
       </div>
-    </>
+    </div>
   );
+
+  function Reset() {
+    setID( Math.floor(Math.random()*3)+1);
+    console.log(ID)
+    setGameState(defaultState);
+  }
+
+  const [GameState, setGameState] = useState(defaultState);
+  return <>{GameState}</>;
 }
 
 export default Game;
